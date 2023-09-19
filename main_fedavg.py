cae652fe9e58a29cd490f00feb3c863c79d87f07
jackhexiangyu12 <22920202204559@stu.xmu.edu.cn>
2023-09-19 09:41:05 +0800

    DMpfl算法一成型，但是准确率不太对，还要改改（40.74））

--- main_fedavg.py
+++ main_fedavg.py
@@ -17,10 +17,6 @@ from models.Nets import MLP, CNNMnist, CNNCifar
 from models.Fed import FedAvg
 from models.test import test_img
 
-def getBinaryTensor(imgTensor, boundary = 200):
-    one = torch.ones_like(imgTensor)
-    zero = torch.zeros_like(imgTensor)
-    return torch.where(imgTensor > boundary, one, zero)
 
 if __name__ == '__main__':
     # parse args
@@ -63,25 +59,9 @@ if __name__ == '__main__':
         exit('Error: unrecognized model')
     print(net_glob)
     net_glob.train()
-    net_glob_client = copy.deepcopy(net_glob)
 
     # copy weights
-    w_g = net_glob.state_dict()
-
-    sparsity_ratio=0.5
-    readjust_masks = True
-    a_s=0.1
-
-    #初始化，把 w_glob中的参数的绝对值中的前sparsity_ratio%的参数置为1，其余的置为0
-    m_g = copy.deepcopy(w_g)
-    for k in m_g.keys():
-        #mid是w_1[k]中绝对值第sparsity_ratio%大的数
-        mid = torch.topk(m_g[k].abs().view(-1), int(sparsity_ratio * m_g[k].numel()))[0][-1]
-        m_g[k] = getBinaryTensor(m_g[k].abs(), boundary=mid)
-
-    #初始化，把 m_c[idx]都设置为m_g
-    m_c = [copy.deepcopy(m_g) for i in range(args.num_users)]
-    w_c = [copy.deepcopy(w_g) for i in range(args.num_users)]
+    w_glob = net_glob.state_dict()
 
     # training
     loss_train = []
@@ -91,80 +71,28 @@ if __name__ == '__main__':
     best_loss = None
     val_acc_list, net_list = [], []
 
-    # if args.all_clients:
-    #     print("Aggregation over all clients")
-    #     w_c = [w_glob for i in range(args.num_users)]
+    if args.all_clients: 
+        print("Aggregation over all clients")
+        w_locals = [w_glob for i in range(args.num_users)]
     for iter in range(args.epochs):
         loss_locals = []
-        # if not args.all_clients:
-        #     w_c = []
+        if not args.all_clients:
+            w_locals = []
         m = max(int(args.frac * args.num_users), 1)
         idxs_users = np.random.choice(range(args.num_users), m, replace=False)
-
         for idx in idxs_users:
             local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
-
-            #对于每一个客户，W_c[idx]中 M_g * M_c[idx]为1的位置= W_g中 M_g * M_c[idx]为1的位置
-            w_c[idx]=copy.deepcopy(w_g)
-            #不能这样，会覆盖
-            # for k in w_c[idx].keys():
-            #     w_c[idx][k] = w_c[idx][k]* m_g[k]
-            #     w_c[idx][k] = w_c[idx][k]* m_c[idx][k]
-
-            for k in w_c[idx].keys():
-                wc_shape = w_c[idx][k].shape
-                wc_next = copy.deepcopy(w_c[idx][k])
-                wc_next = wc_next.reshape(-1)
-                mg = m_g[k].reshape(-1)
-                mc = m_c[idx][k].reshape(-1)
-                wg = w_g[k].reshape(-1)
-
-                # 遍历一维张量
-                dim0 = wc_next.shape
-                for i in range(dim0[0]):
-                    if (mg[i] * mc[i] == 1):
-                        wc_next[i] = wg[i]
-                w_c[idx][k] = wc_next.reshape(wc_shape)
-
-
-            #net_glob[idx]设置为w_c[idx]*m_c[idx]
-            net_glob_client.load_state_dict(w_c[idx])
-            w, loss = local.train(net=copy.deepcopy(net_glob_client).to(args.device))
+            w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
             if args.all_clients:
-                w_c[idx] = copy.deepcopy(w)
+                w_locals[idx] = copy.deepcopy(w)
             else:
-                #
-                w_c.append(copy.deepcopy(w))
+                w_locals.append(copy.deepcopy(w))
             loss_locals.append(copy.deepcopy(loss))
-
-            #判断是否readjust_masks=true:
-            if readjust_masks==True:
-                #对于a_s比例的m_c[idx]中在w_c[idx]中绝对值最小的参数，置为0
-                for k in m_c[idx].keys():
-                    # mid是w_1[k]中绝对值第a_s%小的数
-                    mid = torch.topk(w_c[idx][k].abs().view(-1), int(a_s * w_c[idx][k].numel()),largest=True)[0][-1]
-                    # mid2是w_1[k]中绝对值第a_s%大的数
-                    mid2 = torch.topk(w_c[idx][k].abs().view(-1), int(a_s * w_c[idx][k].numel()),largest=False)[0][-1]
-
-                    wc_shape = w_c[idx][k].shape
-                    wc_next = copy.deepcopy(w_c[idx][k])
-                    wc_next = wc_next.reshape(-1)
-                    mc = m_c[idx][k].reshape(-1)
-
-                    # 遍历一维张量
-                    dim0 = wc_next.shape
-                    for i in range(dim0[0]):
-                        if (wc_next[i]<mid):
-                            mc[i]=0
-                        elif (wc_next[i]>mid2):
-                            mc[i]=1
-                    m_c[idx][k] = mc.reshape(wc_shape)
-
         # update global weights
-        w_g = FedAvg(w_c)
+        w_glob = FedAvg(w_locals)
 
         # copy weight to net_glob
-        net_glob.load_state_dict(w_g)
+        net_glob.load_state_dict(w_glob)
 
         # print loss
         loss_avg = sum(loss_locals) / len(loss_locals)
@@ -183,3 +111,4 @@ if __name__ == '__main__':
     acc_test, loss_test = test_img(net_glob, dataset_test, args)
     print("Training accuracy: {:.2f}".format(acc_train))
     print("Testing accuracy: {:.2f}".format(acc_test))
+
